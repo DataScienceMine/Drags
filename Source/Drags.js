ca2cd7c94596dc192ed89ef473aab48ebc2759d0
HishamElamir <shadow_123413@yahoo.com>
2018-01-04 02:00:20 +0200

    adding new v of codes

--- Source/Drags.js
+++ Source/Drags.js
@@ -10,53 +10,53 @@ var Drags = /** @class */ (function () {
         this.SetContainers = function (containersArray) {
             this.Containers = containersArray;
             for (var i = 0; i < this.Containers.length; i++) {
-                this.Containers[i].ondragenter = this.OnDragEnter;
-                this.Containers[i].ondragover = this.OnDragOver;
-                this.Containers[i].ondragleave = this.OnDragLeave;
-                this.Containers[i].ondrop = this.OnDrop;
+                this.Containers[i].ondragenter = this.OnDragEnter.bind(this);
+                this.Containers[i].ondragover = this.OnDragOver.bind(this);
+                this.Containers[i].ondragleave = this.OnDragLeave.bind(this);
+                this.Containers[i].ondrop = this.OnDrop.bind(this);
             }
         };
         this.OnDragEnter = function (event) {
-            // if(that.MetaData.Debug === true)
-            //     that.MetaData.printFunction('Dragging over the ' + event.target.getAttribute('id'));
+            if (this.MetaData.Debug === true)
+                this.MetaData.printFunction('Dragging over the ' + event.target.getAttribute('id'));
         };
         this.OnDragOver = function (event) {
             return false;
         };
-        this.OnDragLeave = function (event, that) {
-            // if(that.MetaData.Debug === true)
-            //     that.MetaData.printFunction('Leaving the ' + event.target.getAttribute('id'));
+        this.OnDragLeave = function (event) {
+            if (this.MetaData.Debug === true)
+                this.MetaData.printFunction('Leaving the ' + event.target.getAttribute('id'));
         };
         this.OnDrop = function (event) {
             event.preventDefault();
             var element_id = event.dataTransfer.getData('text');
             event.target.appendChild(document.getElementById(element_id));
-            // if(this.MetaData.Debug === true)
-            //     this.MetaData.printFunction('Dropped ' + element_id + ' Over ' + event.target.getAttribute('id'));
+            if (this.MetaData.Debug === true)
+                this.MetaData.printFunction('Dropped ' + element_id + ' Over ' + event.target.getAttribute('id'));
             //_(element_id).removeAttribute('draggable');
             document.getElementById(element_id).style.cursor = 'default';
-            this.dropedIn = true;
+            this.DropIn = true;
         };
         this.SetDraggables = function (elements) {
             this.DraggableObjects = elements;
             for (var i = 0; i < this.DraggableObjects.length; i++) {
                 this.DraggableObjects[i].setAttribute('draggable', true);
-                this.DraggableObjects[i].ondragstart = this.StartDrag;
-                this.DraggableObjects[i].ondragend = this.EndDrag;
+                this.DraggableObjects[i].ondragstart = this.StartDrag.bind(this);
+                this.DraggableObjects[i].ondragend = this.EndDrag.bind(this);
             }
         };
         this.StartDrag = function (event) {
-            // if(this.MetaData.Debug === true)
-            //     this.MetaData.printFunction('Dragging the ' + event.target.getAttribute('id'));
+            if (this.MetaData.Debug === true)
+                this.MetaData.printFunction('Dragging the ' + event.target.getAttribute('id'));
             event.dataTransfer.dropEffect = 'move';
             event.dataTransfer.setData('text', event.target.getAttribute('id'));
         };
         this.EndDrag = function (event) {
-            if (this.dropedIn == false) {
-                // if(this.MetaData.Debug === true)
-                //     this.MetaData.printFunction('You let in ' + event.target.getAttribute('id'));
+            if (this.DropIn == false) {
+                if (this.MetaData.Debug === true)
+                    this.MetaData.printFunction('You let in ' + event.target.getAttribute('id'));
             }
-            this.dropedIn = false;
+            this.DropIn = false;
         };
         this.MetaData = metadata;
     }
--- Source/Drags.ts
+++ Source/Drags.ts
@@ -14,56 +14,55 @@ class Drags {
     public SetContainers = function (containersArray: HTMLElement[]) {
         this.Containers = containersArray;
         for(var i:number = 0; i < this.Containers.length; i++) {
-            this.Containers[i].ondragenter = this.OnDragEnter;
-            this.Containers[i].ondragover = this.OnDragOver;
-            this.Containers[i].ondragleave = this.OnDragLeave;
-            this.Containers[i].ondrop = this.OnDrop;
+            this.Containers[i].ondragenter = this.OnDragEnter.bind(this);
+            this.Containers[i].ondragover = this.OnDragOver.bind(this)
+            this.Containers[i].ondragleave = this.OnDragLeave.bind(this)
+            this.Containers[i].ondrop = this.OnDrop.bind(this);
         }
     }
     private OnDragEnter = function (event) {
-        // if(that.MetaData.Debug === true)
-        //     that.MetaData.printFunction('Dragging over the ' + event.target.getAttribute('id'));
+        if(this.MetaData.Debug === true)
+            this.MetaData.printFunction('Dragging over the ' + event.target.getAttribute('id'));
     };
     private OnDragOver = function (event) {
         return false;
     };
-    private OnDragLeave = function (event, that) {
-        // if(that.MetaData.Debug === true)
-        //     that.MetaData.printFunction('Leaving the ' + event.target.getAttribute('id'));
+    private OnDragLeave = function (event) {
+        if(this.MetaData.Debug === true)
+            this.MetaData.printFunction('Leaving the ' + event.target.getAttribute('id'));
     };
 
     private OnDrop = function (event) {
         event.preventDefault();
         var element_id = event.dataTransfer.getData('text');
         event.target.appendChild(document.getElementById(element_id));
-        // if(this.MetaData.Debug === true)
-        //     this.MetaData.printFunction('Dropped ' + element_id + ' Over ' + event.target.getAttribute('id'));
+        if(this.MetaData.Debug === true)
+            this.MetaData.printFunction('Dropped ' + element_id + ' Over ' + event.target.getAttribute('id'));
         //_(element_id).removeAttribute('draggable');
         document.getElementById(element_id).style.cursor = 'default';
-        this.dropedIn = true;
+        this.DropIn = true;
     };
 
     public SetDraggables = function (elements: HTMLElement[]) {
         this.DraggableObjects = elements;
-
         for(var i:number = 0; i < this.DraggableObjects.length; i++) {
             this.DraggableObjects[i].setAttribute('draggable', true);
-            this.DraggableObjects[i].ondragstart = this.StartDrag;
-            this.DraggableObjects[i].ondragend = this.EndDrag;
+            this.DraggableObjects[i].ondragstart = this.StartDrag.bind(this);
+            this.DraggableObjects[i].ondragend = this.EndDrag.bind(this);
         }
     };
 
     private StartDrag = function (event) {
-        // if(this.MetaData.Debug === true)
-        //     this.MetaData.printFunction('Dragging the ' + event.target.getAttribute('id'));
+        if(this.MetaData.Debug === true)
+            this.MetaData.printFunction('Dragging the ' + event.target.getAttribute('id'));
         event.dataTransfer.dropEffect = 'move';
         event.dataTransfer.setData('text', event.target.getAttribute('id'));
     };
     private EndDrag = function (event) {
-        if(this.dropedIn == false){
-            // if(this.MetaData.Debug === true)
-            //     this.MetaData.printFunction('You let in ' + event.target.getAttribute('id'));
+        if(this.DropIn == false){
+            if(this.MetaData.Debug === true)
+                this.MetaData.printFunction('You let in ' + event.target.getAttribute('id'));
         }
-        this.dropedIn = false;
+        this.DropIn = false;
     };
 };
